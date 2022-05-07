0875412994fb8a7d8f058b59cac680bf8e5ba97e
GHY-ZDL <1320244620@qq.com>
2022-05-07 12:04:48 +0800

    二叉树的前中后序  查找

--- src/main/java/atguigu/tree/BinaryTreeDemo.java
+++ src/main/java/atguigu/tree/BinaryTreeDemo.java
@@ -11,11 +11,11 @@ public class BinaryTreeDemo {
         //先需要创建一个二叉树
         BinaryTree binaryTree = new BinaryTree();
         //创建所需的节点
-        HeroNode root = new HeroNode(1,"宋江");
-        HeroNode node2 = new HeroNode(2,"吴用");
-        HeroNode node3 = new HeroNode(3,"卢俊义");
-        HeroNode node4 = new HeroNode(4,"林冲");
-        HeroNode node5 = new HeroNode(5,"关胜");
+        HeroNode root = new HeroNode(1, "宋江");
+        HeroNode node2 = new HeroNode(2, "吴用");
+        HeroNode node3 = new HeroNode(3, "卢俊义");
+        HeroNode node4 = new HeroNode(4, "林冲");
+        HeroNode node5 = new HeroNode(5, "关胜");
 
         //说明，我们先手动创建该二叉树，后面我们学习以递归的方式创建二叉树
         binaryTree.setRoot(root);
@@ -37,11 +37,16 @@ public class BinaryTreeDemo {
         System.out.println("测试后序遍历");
         binaryTree.houOrder();   //25431
 
+        binaryTree.qianOrderSearch(5 );
+        binaryTree.zhongOrderSearch( 5);
+        binaryTree.houOrderSearch( 5);
+
     }
 
 }
+
 //定义二叉树 确定了一个根节点就确定了一颗二叉树
-class BinaryTree{
+class BinaryTree {
     private HeroNode root;
 
     //提供一个set方法，方便后续将某个节点定义为根节点
@@ -50,27 +55,80 @@ class BinaryTree{
         this.root = root;
     }
 
-    public void qianOrder(){
-        if (this.root!=null){
+    public void qianOrder() {
+        if (this.root != null) {
             this.root.preOrder();
-        }else {
+        } else {
             System.out.println("当前二叉树为空！");
         }
     }
-    public void zhongOrder(){
-        if (this.root!=null){
+
+    public HeroNode qianOrderSearch(int id) {
+        if (this.root != null) {
+            HeroNode rsHeroNode = this.root.preOrderSearch(id);
+            if (rsHeroNode != null) {
+                System.out.println("前序查找找到的节点信息为：" + rsHeroNode);
+                return rsHeroNode;
+            } else {
+                System.out.println("当前二叉树中没有id为" + id + "的节点");
+                return null;
+            }
+        } else {
+            System.out.println("当前二叉树为空！");
+        }
+
+        return null;
+    }
+
+    public void zhongOrder() {
+        if (this.root != null) {
             this.root.midOrder();
-        }else {
+        } else {
             System.out.println("当前二叉树为空！");
         }
     }
-    public void houOrder(){
-        if (this.root!=null){
+
+    public HeroNode zhongOrderSearch(int id) {
+        if (this.root != null) {
+            HeroNode rsHeroNode = this.root.midOrderSearch(id);
+            if (rsHeroNode != null) {
+                System.out.println("中序查找找到的节点信息为：" + rsHeroNode);
+                return rsHeroNode;
+            } else {
+                System.out.println("当前二叉树中没有id为" + id + "的节点");
+                return null;
+            }
+        } else {
+            System.out.println("当前二叉树为空！");
+        }
+
+        return null;
+    }
+
+    public void houOrder() {
+        if (this.root != null) {
             this.root.postOrder();
-        }else {
+        } else {
             System.out.println("当前二叉树为空！");
         }
     }
+
+    public HeroNode houOrderSearch(int id) {
+        if (this.root != null) {
+            HeroNode rsHeroNode = this.root.postOrderSearch(id);
+            if (rsHeroNode != null) {
+                System.out.println("后续查找找到的节点信息为：" + rsHeroNode);
+                return rsHeroNode;
+            } else {
+                System.out.println("当前二叉树中没有id为" + id + "的节点");
+                return null;
+            }
+        } else {
+            System.out.println("当前二叉树为空！");
+        }
+        return null;
+    }
+
 }
 
 
@@ -96,6 +154,31 @@ class HeroNode {
         }
     }
 
+    public HeroNode preOrderSearch(int id) {
+        if (this.id == id) {
+            return this;
+        }
+        //用来记录查找的返回值
+        HeroNode rsHeroNode = null;
+        if (this.left != null) {
+            rsHeroNode = this.left.preOrderSearch(id);
+            //查找的返回值不为null（说明在当前的左子树中找到了），继续向上级树返回结果；如返回null，则查找继续进行
+            if (rsHeroNode != null) {
+                return rsHeroNode;
+            }
+        }
+        if (this.right != null) {
+            rsHeroNode = this.right.preOrderSearch(id);
+            //查找的返回值不为null（说明在当前的右子树中找到了），继续向上级树返回结果；如返回null，则查找继续进行（实际该右子树的查找已经结束了）
+            //无论是否为null，下一步紧跟着都要返回，所以直接省略这步判断，向上返回节点即可
+            /*if (rsHeroNode!=null){
+                return rsHeroNode;
+            }*/
+        }
+        //运行到此，说明该子树内部已经查找完成，无论结果是否为null都要向上返回值
+        return rsHeroNode;
+    }
+
     public void midOrder() {
 
         if (this.left != null) {
@@ -107,6 +190,31 @@ class HeroNode {
         }
     }
 
+    public HeroNode midOrderSearch(int id) {
+        //用来记录查找的返回值
+        HeroNode rsHeroNode = null;
+        if (this.left != null) {
+            rsHeroNode = this.left.midOrderSearch(id);
+            //查找的返回值不为null（说明在当前的左子树中找到了），继续向上级树返回结果；如返回null，则查找继续进行
+            if (rsHeroNode != null) {
+                return rsHeroNode;
+            }
+        }
+        if (this.id == id) {
+            return this;
+        }
+        if (this.right != null) {
+            rsHeroNode = this.right.midOrderSearch(id);
+            //查找的返回值不为null（说明在当前的右子树中找到了），继续向上级树返回结果；如返回null，则查找继续进行（实际该右子树的查找已经结束了）
+            //无论是否为null，下一步紧跟着都要返回，所以直接省略这步判断，向上返回节点即可
+            /*if (rsHeroNode!=null){
+                return rsHeroNode;
+            }*/
+        }
+        //运行到此，说明该子树内部已经查找完成，无论结果是否为null都要向上返回值
+        return rsHeroNode;
+    }
+
     public void postOrder() {
 
         if (this.left != null) {
@@ -116,6 +224,31 @@ class HeroNode {
             this.right.postOrder();
         }
         System.out.println(this);
+    }
+
+    public HeroNode postOrderSearch(int id) {
+        //用来记录查找的返回值
+        HeroNode rsHeroNode = null;
+        if (this.left != null) {
+            rsHeroNode = this.left.postOrderSearch(id);
+            //查找的返回值不为null（说明在当前的左子树中找到了），继续向上级树返回结果；如返回null，则查找继续进行
+            if (rsHeroNode != null) {
+                return rsHeroNode;
+            }
+        }
+        if (this.right != null) {
+            rsHeroNode = this.right.postOrderSearch(id);
+            //查找的返回值不为null（说明在当前的右子树中找到了），继续向上级树返回结果；如返回null，则查找继续进行
+            if (rsHeroNode != null) {
+                return rsHeroNode;
+            }
+        }
+
+        if (this.id == id) {
+            return this;
+        }
+        //运行到此，说明该子树内部已经查找完成，无论结果是否为null都要向上返回值
+        return null;
     }
 
     @Override
